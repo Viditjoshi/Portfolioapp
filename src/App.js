1cdc4619cc4cff79c4a9d326d7a28ab0031534cc
vidit joshi <viditjoshi94@gmail.com>
2024-05-14 16:04:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./index.css";
 import Home from './router/Home.js';
 import About from './router/About.js';
 import Project from './router/Project.js';
-import Contact from './router/Contact,js';
+import Contact from './router/Contact.js';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Routes>
-          <Route path='/home' element={<Home/>}/>
+          <Route path='/home' element={<Home/>} />
           <Route path='/About' element={<About/>}/>
           <Route path='/Project' element={<Project/>}/>
           <Route path='/Contact' element={<Contact/>}/>
